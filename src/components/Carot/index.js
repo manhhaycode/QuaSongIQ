8cdf277c3f0b5f2556a86171de1ed42ffb9d0a6c
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-03-14 14:30:46 +0700

    upload

--- src/components/Carot/index.js
+++ src/components/Carot/index.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { states, actions } from '~/recoil';
 import images from '~/assets/image';
+import { useEffect } from 'react';
 const cx = classNames.bind(styles);
 
 export default function Carot() {
@@ -10,35 +11,50 @@ export default function Carot() {
     const val = useRecoilValue(states.gameState);
 
     const handleClickCarot = () => {
-        if (!val.carot.onBoat.status) {
-            if (val.seatEmpty.first) {
-                stateMovement(actions.setCarotOnBoat({ status: true, pos: 1 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: false }));
-            } else if (val.seatEmpty.second) {
-                stateMovement(actions.setCarotOnBoat({ status: true, pos: 2 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: false }));
-            }
-        } else {
-            if (val.carot.onBoat.pos === 1) {
-                stateMovement(actions.setCarotOnBoat({ status: false, pos: 0 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: true }));
-            } else if (val.carot.onBoat.pos === 2) {
-                stateMovement(actions.setCarotOnBoat({ status: false, pos: 0 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: true }));
+        if (val.carot.river === val.raft.pos) {
+            if (!val.carot.onBoat.status) {
+                if (val.seatEmpty.first) {
+                    stateMovement(actions.setCarotOnBoat({ status: true, pos: 1 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: false }));
+                } else if (val.seatEmpty.second) {
+                    stateMovement(actions.setCarotOnBoat({ status: true, pos: 2 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: false }));
+                }
+            } else {
+                if (val.carot.onBoat.pos === 1) {
+                    stateMovement(actions.setCarotOnBoat({ status: false, pos: 0 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: true }));
+                } else if (val.carot.onBoat.pos === 2) {
+                    stateMovement(actions.setCarotOnBoat({ status: false, pos: 0 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: true }));
+                }
             }
         }
     };
+
+    useEffect(() => {
+        if (val.carot.onBoat.status && val.raft.pos !== val.carot.river) {
+            stateMovement(actions.setCarotRiver(val.raft.pos));
+        }
+        // eslint-disable-next-line
+    }, [val.raft.pos]);
 
     return (
         <img
             alt=""
             src={images.carot}
             className={
-                !val.carot.onBoat.status
-                    ? cx('carot-img')
+                val.carot.river === 1
+                    ? !val.carot.onBoat.status
+                        ? cx('carot-img')
+                        : val.carot.onBoat.pos === 1
+                        ? cx('carot-img', 'carot-onboat1')
+                        : cx('carot-img', 'carot-onboat2')
+                    : !val.carot.onBoat.status
+                    ? cx('carot-img2')
                     : val.carot.onBoat.pos === 1
-                    ? cx('carot-img', 'carot-onboat1')
-                    : cx('carot-img', 'carot-onboat2')
+                    ? cx('carot-img', 'carot-onboat12')
+                    : cx('carot-img', 'carot-onboat22')
             }
             onClick={handleClickCarot}
             draggable="false"
--- src/components/Sheep/index.js
+++ src/components/Sheep/index.js
@@ -3,46 +3,66 @@ import classNames from 'classnames/bind';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { actions, states } from '~/recoil';
 import images from '~/assets/image';
+import { useEffect, useRef } from 'react';
 
 const cx = classNames.bind(styles);
 
 export default function Sheep() {
     const stateMovement = useSetRecoilState(states.handleGameState);
     const val = useRecoilValue(states.gameState);
+    const refSheep = useRef();
 
     const handleClickSheep = () => {
-        if (!val.sheep.onBoat.status) {
-            if (val.seatEmpty.first) {
-                stateMovement(actions.setSheepOnBoat({ status: true, pos: 1 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: false }));
-            } else if (val.seatEmpty.second) {
-                stateMovement(actions.setSheepOnBoat({ status: true, pos: 2 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: false }));
-            }
-        } else {
-            if (val.sheep.onBoat.pos === 1) {
-                stateMovement(actions.setSheepOnBoat({ status: false, pos: 0 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: true }));
-            } else if (val.sheep.onBoat.pos === 2) {
-                stateMovement(actions.setSheepOnBoat({ status: false, pos: 0 }));
-                stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: true }));
+        if (val.sheep.river === val.raft.pos) {
+            if (!val.sheep.onBoat.status) {
+                if (val.seatEmpty.first) {
+                    stateMovement(actions.setSheepOnBoat({ status: true, pos: 1 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: false }));
+                } else if (val.seatEmpty.second) {
+                    stateMovement(actions.setSheepOnBoat({ status: true, pos: 2 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: false }));
+                }
+            } else {
+                if (val.sheep.onBoat.pos === 1) {
+                    stateMovement(actions.setSheepOnBoat({ status: false, pos: 0 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, first: true }));
+                } else if (val.sheep.onBoat.pos === 2) {
+                    stateMovement(actions.setSheepOnBoat({ status: false, pos: 0 }));
+                    stateMovement(actions.setSeatEmpty({ ...val.seatEmpty, second: true }));
+                }
             }
         }
     };
 
+    useEffect(() => {
+        if (val.sheep.onBoat.status && val.raft.pos !== val.sheep.river) {
+            stateMovement(actions.setSheepRiver(val.raft.pos));
+        }
+        // eslint-disable-next-line
+    }, [val.raft.pos]);
+
     return (
         <img
             alt=""
-            src={images.sheep0}
+            src={val.sheep.river === 1 ? images.sheep0 : images.sheep1}
             className={
-                !val.sheep.onBoat.status
-                    ? cx('sheep-img')
+                val.sheep.river === 1
+                    ? !val.sheep.onBoat.status
+                        ? cx('sheep-img')
+                        : val.sheep.onBoat.pos === 1
+                        ? cx('sheep-img', 'sheep-onboat1')
+                        : cx('sheep-img', 'sheep-onboat2')
+                    : !val.sheep.onBoat.status
+                    ? cx('sheep-img2')
                     : val.sheep.onBoat.pos === 1
-                    ? cx('sheep-img', 'sheep-onboat1')
-                    : cx('sheep-img', 'sheep-onboat2')
+                    ? cx('sheep-img', 'sheep-onboat12')
+                    : cx('sheep-img', 'sheep-onboat22')
             }
             onClick={handleClickSheep}
             draggable="false"
-        ></img>
+            ref={refSheep}
+        >
+            {console.log(val)}
+        </img>
     );
 }
